6aec0dc349aa08bb90ed25712afc861e46a873b6
junhok82 <junhok82@gmail.com>
2020-06-09 21:08:37 +0900

    preparation : 리그 정보 파싱
    1. Delete
       -
    
    2. Create
       - LeagueEntry.java : 리그 정보 DTO
       - LeagueEntryParser.java : 리그 정보 파싱
    
    3. Update
       - Controller : 리그 정보 파싱을 위한 작업

--- SpringGG/src/main/java/com/junho/controller/Controller.java
+++ SpringGG/src/main/java/com/junho/controller/Controller.java
@@ -1,7 +1,5 @@
 package com.junho.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,16 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.junho.dto.ChampionLotation;
-import com.junho.dto.ChampionMastery;
-import com.junho.dto.CurrentGameInfo;
-import com.junho.dto.MatchList;
+import com.junho.dto.LeagueEntry;
 import com.junho.dto.Summoner;
 import com.junho.service.SummonerService;
-import com.junho.util.ChampionLotationParser;
-import com.junho.util.ChampionMasteryParser;
-import com.junho.util.CurrentGameInfoParser;
-import com.junho.util.MatchListParser;
+import com.junho.util.LeagueEntryParser;
 import com.junho.util.SummonerParser;
 
 @RestController
@@ -48,28 +40,32 @@ public class Controller {
 			}
 		}
 		
+		// 리그 정보
+		LeagueEntryParser leagueEntryParser = new LeagueEntryParser();
+		LeagueEntry leagueEntry = leagueEntryParser.getJsonData(summoner.getId());
+		System.out.println("리그 엔트리 : " + leagueEntry.toString());
 		
 		// 숙련도 정보 
-		String summonerid = summoner.getId();
-		ChampionMasteryParser championMasteryParser = new ChampionMasteryParser();
-		List<ChampionMastery> championMasteryList = championMasteryParser.getJsonData(summonerid);
+		//String summonerid = summoner.getId();
+		//ChampionMasteryParser championMasteryParser = new ChampionMasteryParser();
+		//List<ChampionMastery> championMasteryList = championMasteryParser.getJsonData(summonerid);
 		
 		// 로테이션 정보
-		ChampionLotationParser championLotationParser = new ChampionLotationParser();
-		ChampionLotation championLotation = championLotationParser.getJsonData();		
-		System.out.println("챔피언 로테이션 정보 : " + championLotation.toString());
+		//ChampionLotationParser championLotationParser = new ChampionLotationParser();
+		//ChampionLotation championLotation = championLotationParser.getJsonData();		
+		//System.out.println("챔피언 로테이션 정보 : " + championLotation.toString());
 		
 		// 전적 리스트
-		String accountId = summoner.getAccountId();
-		MatchListParser matchListParser = new MatchListParser();
-		MatchList matchList = matchListParser.getJsonData(accountId);
-		System.out.println("전적 리스트 : " + matchList.toString());
+		//String accountId = summoner.getAccountId();
+		//MatchListParser matchListParser = new MatchListParser();
+		//MatchList matchList = matchListParser.getJsonData(accountId);
+		//System.out.println("전적 리스트 : " + matchList.toString());
 		
 		
 		// 현재 게임정보
-		CurrentGameInfoParser currentGameInfoParser = new CurrentGameInfoParser();
-		CurrentGameInfo currentGameInfo = currentGameInfoParser.getJsonData(summonerid);
-		System.out.println("현재 게임 정보 : " + currentGameInfo.toString());
+		// CurrentGameInfoParser currentGameInfoParser = new CurrentGameInfoParser();
+		// CurrentGameInfo currentGameInfo = currentGameInfoParser.getJsonData(summonerid);
+		// System.out.println("현재 게임 정보 : " + currentGameInfo.toString());
 		
 		if(summoner != null && !summoner.equals(null)) {
 			return new ResponseEntity<Summoner>(summoner,HttpStatus.OK);
